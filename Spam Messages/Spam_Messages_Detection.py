edaf38db465096063a747d3c42907a624d0e7b48
dks2000dks <srikarkrishnad@gmail.com>
2020-01-19 12:18:20 +0530

    Improved Accuracy

--- Spam Messages/Spam_Messages_Detection.py
+++ Spam Messages/Spam_Messages_Detection.py
@@ -1,3 +1,5 @@
+# Dataset is avaible in Kaggle
+
 import pandas as pd
 import numpy as np
 import seaborn as sns
@@ -146,7 +148,7 @@ model.add(Dense(1, activation='sigmoid'))
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 model.summary()
 # Fit the model
-model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=2, batch_size=128, verbose=2)
+model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=10, batch_size=128, verbose=2)
 # Final evaluation of the model
 scores = model.evaluate(X_test, y_test, verbose=0)
 print("Accuracy: %.2f%%" % (scores[1]*100))
